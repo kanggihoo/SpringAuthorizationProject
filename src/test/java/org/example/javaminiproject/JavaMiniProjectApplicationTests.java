c670977b9b6f08776c6b59d7af5a0596ec60520b
kkh <11kkh19999@gmail.com>
2026-02-20 14:52:02 +0900

    project setting with gradle

---
+++ src/test/java/org/example/javaminiproject/JavaMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
