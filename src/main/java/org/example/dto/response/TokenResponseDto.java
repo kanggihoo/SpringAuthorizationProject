b8720db3f573006854a792345bfe5dd325687659
kkh <11kkh19999@gmail.com>
2026-03-03 03:26:39 +0900

    feat: add Swagger Schema annotations to DTOs

--- src/main/java/org/example/dto/response/TokenResponseDto.java
+++ src/main/java/org/example/dto/response/TokenResponseDto.java
@@ -5,36 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "인증 토큰 응답 데이터")
 @Getter
 @Builder
 public class TokenResponseDto {
+  @Schema(description = "Access Token", example = "eyJhbGciOiJIUzI1NiJ9...")
   private String accessToken;
+  
+  @Schema(description = "토큰 타입", example = "Bearer")
   private String tokenType;
   
 
   @JsonIgnore
-    
-  
-
   private String refreshToken;
-    
-  
 }
-
-    
-  
-    
-  
-      
-      
-    
-
-    
-      
-      
-    
-
-    
-      
-      
-    
